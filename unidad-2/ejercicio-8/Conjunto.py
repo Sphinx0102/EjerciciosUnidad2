2f23f686de4f747baf97eb4a3030c0a4fa49acd3
Sphinx0102 <leo.sebas.lr@gmail.com>
2022-05-07 01:21:44 -0300

    poo-ejercicios

--- unidad-2/ejercicio-8/Conjunto.py
+++ unidad-2/ejercicio-8/Conjunto.py
@@ -1,6 +1,35 @@
+import collections
+
+
 class Conjunto():
-    __valor = None
+    __arreglo = []
 
 
-def __init__(self, valor = ''):
-    self.__valor = int(valor)
+    def __init__(self, array = []):
+        self.__arreglo = array
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def get(self):
+        return self.__arreglo
+
+    def __add__(self, array2):
+        array1=self.__arreglo
+        array1.extend(array2.get())
+        return list(set(array1))
+
+    
+    def __sub__(self, array2):
+        array1=self.__arreglo
+        diferencia1 = (set(array1).difference(array2.get()))
+        diferencia2 = (set(array2.get()).difference(array1))
+        list_differencia = list(diferencia1.union(diferencia2))
+        return list_differencia
+
+    def __eq__(self, array2):
+        array1=self.__arreglo
+        if(collections.Counter(array1) == collections.Counter(array2.get())):
+            return True
+        else:
+            return False
--- unidad-2/ejercicio-8/main.py
+++ unidad-2/ejercicio-8/main.py
@@ -1,13 +1,27 @@
 from Conjunto import Conjunto
+from claseMenu import Menu
 
-
-def carga_conjunto():
     
 
 
 def main():
-    carga_conjunto()
+    
+    A= Conjunto([1,2,3,4])
+    B = Conjunto([4,6,9])
 
+
+
+    bandera = False
+    while not bandera:
+        print("""Menu: 
+            Opcion 1: La unión de dos conjuntos
+            Opcion 2: La diferencia de dos conjuntos
+            Opcion 3: Verificar si dos conjuntos son iguales
+            Opcion 4: Salir""")
+        opcion = input("ingrese el numero de la opcion deseada: ")
+        menu = Menu()
+        menu.opcion(opcion, A, B)
+        bandera = int(opcion) == 4
 
 
 if __name__=='__main__':
